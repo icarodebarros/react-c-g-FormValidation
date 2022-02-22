a49e0c2c183e783921ac9d07162ac42656cb1c1a
Icaro de Barros <icarodebarros@gmail.com>
2022-02-22 11:17:24 -0300

    Manual validation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,11 +1,42 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+  const [enteredFirstName, setEnteredFirstName] = useState('');
+  const [firstNameIsTouched, setFirstNameIsTouched] = useState(false);
+  
+  const firstNameIsValid = enteredFirstName.trim() !== '';
+
+  const changeFirstNameHandler = (event) => {
+    setEnteredFirstName(event.target.value);
+    setFirstNameIsTouched(true);
+  }
+
+  const onBlurFirstNameHandler = (event) => {
+    setFirstNameIsTouched(true);
+  }
+
+  const firstNameInputIsValid = !firstNameIsTouched || firstNameIsValid;
+  const inputFirstNameClasses = firstNameInputIsValid ? 'form-control' : 'form-control invalid';
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if(!firstNameInputIsValid) return;
+
+    console.log(enteredFirstName)
+    setEnteredFirstName('');
+    setFirstNameIsTouched(false);
+  }
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={inputFirstNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' value={enteredFirstName} onChange={changeFirstNameHandler} onBlur={onBlurFirstNameHandler} />
         </div>
+        {!firstNameInputIsValid && (
+          <p className='error-text'>Name must not be empty.</p>
+        )}
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' />
